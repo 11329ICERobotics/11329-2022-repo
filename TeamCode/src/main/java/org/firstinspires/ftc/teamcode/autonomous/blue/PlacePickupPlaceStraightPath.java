5699bfac5aed5427702d278aa7253ff07b4c3f2a
SB510 <sean.borneman@gmail.com>
2022-03-12 06:55:42 -0500

    autos for STATE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/blue/PlacePickupPlaceStraightPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/blue/PlacePickupPlaceStraightPath.java
@@ -13,8 +13,8 @@ import org.firstinspires.ftc.teamcode.commands.autonav.tasks.WaitTask;
 import org.firstinspires.ftc.teamcode.commands.autonav.tasks.toDistanceTask;
 import org.firstinspires.ftc.teamcode.utilities.RobotSide;
 
-//Mirroed from RED NOT TESTED
-@Autonomous(name="Place, Pickup Another,Straight Blue", group="Blue")
+//LAST ON XPosition was commented (Done, not tuned)
+@Autonomous(name="Place, Pickup Another,Straight Blue ", group="Blue")
 public class PlacePickupPlaceStraightPath extends AutoBase {
     @Override
     public void Run() {
@@ -45,11 +45,11 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
                         //dropNewFreightAndPark();
                         break;
                     case Three:
-                        autoNav.AddTask(new MovementTask(300, 0, 0, 1f));
+                        autoNav.AddTask(new MovementTask(550, 0, 0, 0.75f));
                         ThirdPosition();
-                        Pickup();
+                        SpecialPickup();
                         FinalThirdPosition();
-                        Park();
+                        SpecialPark();
                         //dropNewFreightAndPark();
                         break;
                     case Reading:
@@ -57,6 +57,7 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
                         ThirdPosition();
                         Pickup();
                         FinalThirdPosition();
+                        Park();
                         //dropNewFreightAndPark();
                         break;
                 }
@@ -74,6 +75,14 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
             }
         });
     }
+    public void SpecialPark(){
+        autoNav.AddTask(new ArmTask(1000, null));
+
+        autoNav.AddTask(new MovementTask(1000,-1, 1f));
+        autoNav.AddTask(new MovementTask(450, -90, 0, 0.8f));
+        autoNav.AddTask(new MovementTask(2500, 0, 0, 0.8f));
+        //autoNav.AddTask(new MovementTask(1500, 0, 0, 1f));
+    }
     public void Park() {
         telemetry.log().add("started PARK");
         autoNav.AddTask(new ArmTask(1000, null));
@@ -84,8 +93,9 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
         autoNav.AddTask(new MovementTask(700, 180, 0, 1f));*/
         //autoNav.AddTask(new MovementTask(500, 0, -1, 1f));
         //autoNav.AddTask(new MovementTask(1000, 0, 0, 0.5f));
+
         autoNav.AddTask(new MovementTask(1000,-1, 1f));
-        autoNav.AddTask(new MovementTask(2500, 0, 0, 1f));
+        autoNav.AddTask(new MovementTask(2500, 0, 0, 0.8f));
         //autoNav.AddTask(new MovementTask(1500, 0, 0, 1f));
 
     }
@@ -93,14 +103,15 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
         autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontThird, RobotConfig.intakeSpeed));
         autoNav.AddTask(new WaitTask(500));
         autoNav.AddTask(new ArmTask(null, 0.0));
-        autoNav.AddTask(new MovementTask(150, 0, 0, 1f));
+        //autoNav.AddTask(new MovementTask(150, 0, 0, 1f));
         autoNav.AddTask(new MovementTask(5700, -90, 0, 0.25f));
 
         autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontThird, -RobotConfig.outtakeSpeed));
         autoNav.AddTask(new WaitTask(2000));
-        autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontThird, 0.0));
+
 
         autoNav.AddTask(new MovementTask(800, 90, 0, 1f));
+        autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontFirst, 0.0));
         autoNav.AddTask(new MovementTask(1200,-1, 1f));
         autoNav.AddTask(new MovementTask(1000, -90, 0, 1f));
         autoNav.AddTask(new MovementTask(350, 0, 0, 1f));
@@ -110,11 +121,12 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
         autoNav.AddTask(new WaitTask(500));
         autoNav.AddTask(new ArmTask(null, 0.0));
         autoNav.AddTask(new MovementTask(5700, -90, 0, 0.25f));
-        autoNav.AddTask(new MovementTask(250, 0, 0, 1f));
+        autoNav.AddTask(new MovementTask(250, 0, 0, 0.5f));
         autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontSecond, -RobotConfig.outtakeSpeed));
         autoNav.AddTask(new WaitTask(2000));
-        autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontSecond, 0.0));
+
         autoNav.AddTask(new MovementTask(300, 180, 0, 1f));
+        autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontFirst, 0.0));
         autoNav.AddTask(new MovementTask(800, 90, 0, 1f));
         autoNav.AddTask(new MovementTask(1200,-1, 1f));
         autoNav.AddTask(new MovementTask(1000, -90, 0, 1f));
@@ -128,13 +140,14 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
         autoNav.AddTask(new ArmTask(null, 0.0));
         autoNav.AddTask(new MovementTask(5700, -90, 0, 0.25f));
 
-        autoNav.AddTask(new MovementTask(250, 0, 0, 1f));
+        //autoNav.AddTask(new MovementTask(250, 0, 0, 1f));
         autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontFirst, -RobotConfig.outtakeSpeed));
         autoNav.AddTask(new WaitTask(1000));
+
+        //autoNav.AddTask(new MovementTask(300, -180, 0, 1f));
         autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontFirst, 0.0));
-        autoNav.AddTask(new MovementTask(300, 180, 0, 1f));
         autoNav.AddTask(new MovementTask(800, 90, 0, 1f));
-        autoNav.AddTask(new MovementTask(1200,1, 1f));
+        autoNav.AddTask(new MovementTask(1200,-1, 1f));
         autoNav.AddTask(new MovementTask(1000, -90, 0, 1f));
     }
     public void FinalThirdPosition() {
@@ -142,8 +155,8 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
         autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontFirst, RobotConfig.intakeSpeed));
         //autoNav.AddTask(new WaitTask(500));
         autoNav.AddTask(new ArmTask(null, 0.0));
-        autoNav.AddTask(new MovementTask(500, 0, 0, 1f));
-        //autoNav.AddTask(new MovementTask(50, 0, 10, 0.5f));
+        autoNav.AddTask(new MovementTask(950, 0, 0, 0.5f));
+        //autoNav.AddTask(new MovementTas/k(50, 0, 10, 0.5f));
         autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.frontFirst, -RobotConfig.outtakeSpeed));
         autoNav.AddTask(new WaitTask(1500));
         //autoNav.AddTask(new MovementTask(50, 0, 10, 0.5f));
@@ -159,7 +172,7 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
 
         autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.intake, RobotConfig.intakeSpeed));
         //autoNav.AddTask(new toDistanceTask(50, 1f));
-        autoNav.AddTask(new MovementTask(1400, -10, 0, 1f));
+        autoNav.AddTask(new MovementTask(1300, -10, 0, 0.8f));
         autoNav.AddTask(new ArmTask(1200, RobotConfig.intakeSpeed));
         //autoNav.AddTask(new WaitTask(2500));
         autoNav.AddTask(new toDistanceTask(100, 0.7f));
@@ -170,12 +183,32 @@ public class PlacePickupPlaceStraightPath extends AutoBase {
 
         autoNav.AddTask(new ArmTask(1200, null));
         autoNav.AddTask(new MovementTask(1300, 90, 0, 0.5f));
-        autoNav.AddTask(new MovementTask(950,-1, -1f));
+        autoNav.AddTask(new MovementTask(925,-1, -1f));
+    }
+    public void SpecialPickup() {
+        telemetry.log().add("started PICKUP");
+        autoNav.AddTask(new ArmTask(1200, null));
+
+
+        autoNav.AddTask(new ArmTask(RobotConfig.ArmPresets.intake, RobotConfig.intakeSpeed));
+        //autoNav.AddTask(new toDistanceTask(50, 1f));
+        autoNav.AddTask(new MovementTask(1400, -10, 0, 0.8f));
+        autoNav.AddTask(new ArmTask(1200, RobotConfig.intakeSpeed));
+        //autoNav.AddTask(new WaitTask(2500));
+        autoNav.AddTask(new toDistanceTask(100, 0.7f));
+
+        autoNav.AddTask(new MovementTask(550, -170, 0, 1f));
+
+        //Get ready to place again
+
+        autoNav.AddTask(new ArmTask(1200, null));
+        autoNav.AddTask(new MovementTask(1300, 90, 0, 0.5f));
+        autoNav.AddTask(new MovementTask(925,-1, -1f));
     }
     public void PickupFar() {
         telemetry.log().add("started PICKUP");
         autoNav.AddTask(new ArmTask(1200, null));
-        autoNav.AddTask(new MovementTask(850,-1, 1f));
+        autoNav.AddTask(new MovementTask(850,1, 1f));
         autoNav.AddTask(new MovementTask(800, -90, 0, 1f));
         //autoNav.AddTask(new MovementTask(800, -90, 0, 1f));
 
